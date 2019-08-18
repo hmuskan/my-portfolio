d10136889a0b0c4c8990f8dfcac81f52008d89e1
hmuskan <muskan.huss@gmail.com>
2019-08-18 16:46:00 +0530

    Edit Experience Section

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -17,40 +17,30 @@ export default class Experience extends Component {
                <h4><b>Summer Intern</b></h4>
                <h5 className="font-yellow"><b>Ministry of Electronics and Information Technology, Govt. of India</b></h5>
                <h6 className="margin-t-10">JUNE 2019 - JULY 2019</h6>
-               <p className="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales.
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,
-                 quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                 eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-               <ul className="list margin-b-30">
-                 <li>Selected under Digital India Internship Scheme - 2019, under
-the field of Mobile Governance</li>
-                 <li>Developed API and Frontend for the Pull Aborted Database
-for DigiLocker</li>
+               <p className="font-semi-white margin-tb-30">Selected under Digital India Internship Scheme - 2019, under
+the field of Mobile Governance, and underwent a training in git fundamentals, Python, MongoDB and API Development using Python Eve. Developed API and Frontend for the Pull Aborted Database
+for DigiLocker, a secure cloud based platform for storage, sharing and verification of documents & certificates, conceived by the National e-Governance Division.
+</p>
 
-               </ul>
              </div>{/* experience */}
              <div className="experience margin-b-50">
                <h4><b>Intern Leader</b></h4>
                <h5 className="font-yellow"><b>The Sparks Foundation</b></h5>
                <h6 className="margin-t-10">JUNE 2019 - AUGUST 2019</h6>
-               <p className="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales.
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,
-                 quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                 eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+               <p className="font-semi-white margin-tb-30">This was an extension to the internship at the same organisation, on the basis of my performance, I was asked to guide interns in the
+               subsequent batches, abbout the app development task. I was responsible for taking sessions on Android App Development, and solving their doubts.</p>
                <ul className="list margin-b-30">
-                 <li>Duis non volutpat arcu, eu mollis tellus.</li>
-                 <li>Quis consequat nulla effi citur at.</li>
-                 <li>Sed finibus aliquam neque sit.</li>
+                 <li>Received Letter of Recommendation</li>
+                 <li>Solved doubts of 10+ interns, and took sessions on Networking in Android using the Volley Library.</li>
+
                </ul>
              </div>{/* experience */}
              <div className="experience margin-b-50">
                <h4><b>Android Development Intern</b></h4>
                <h5 className="font-yellow"><b>The Sparks Foundation</b></h5>
                <h6 className="margin-t-10">APRIL 2019 - JUNE 2019</h6>
-               <p className="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales.
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit,
-                 quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu,
-                 eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+               <p className="font-semi-white margin-tb-30">The company organises Graduate Rotational Internship Program, where
+students get a chance to get mentored in their field of interest. As an intern here, I developed 2 Android apps in each phase.</p>
                <ul className="list margin-b-30">
                  <li>Phase 1: Created a credit management app, which allows
 simple transfer of credit points between users</li>
