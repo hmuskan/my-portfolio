6233633da24c5468edf37b589e75f6c5eed68214
hmuskan <muskan.huss@gmail.com>
2019-08-19 23:40:31 +0530

    Updates

---
+++ precache-manifest.d8713ec4ede89441522dcc9adff6fb8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29348344d5286e2de71eb6f6d1835389",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d31a9b60b13873c79c93",
+    "url": "/my-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dc9d02950c3383526f22",
+    "url": "/my-portfolio/static/js/2.e04600d8.chunk.js"
+  },
+  {
+    "revision": "d31a9b60b13873c79c93",
+    "url": "/my-portfolio/static/js/main.f06726ec.chunk.js"
+  },
+  {
+    "revision": "c28e1d0b694ae15ffcf3",
+    "url": "/my-portfolio/static/js/runtime~main.6dd1a59d.js"
+  }
+]);
